dc86aa586a87a6730f98cb16404c277bd8ed7859
BlessedNur <blessednur67@gmail.com>
2025-02-01 07:16:06 +0100

    1st_commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,16 @@ const compat = new FlatCompat({
   baseDirectory: __dirname,
 });
 
-const eslintConfig = [...compat.extends("next/core-web-vitals")];
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      "react/no-unescaped-entities": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@next/next/no-img-element": "warn",
+    },
+  },
+];
 
 export default eslintConfig;
